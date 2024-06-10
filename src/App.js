9a9580463a587cd9a3342126665bede46e5e46f6
Your Name <you@example.com>
2024-06-10 15:44:05 -0400

    git init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="d1ccbe12-0f92-4e75-96b6-97fda1d64f1f"
+            userName="woody"
+            userSecret="123123"
+        />
+    );
 }
 
-export default App;
+export default App; 
